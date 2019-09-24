58813d015824a15c20f0fb598c3dcd7dc1f57761
SurplusSineWaves <surplussinewaves@gmail.com>
2019-09-24 17:44:13 +0100

    Incremented version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='fcards',
-    version='0.3.0',
+    version='0.3.1',
     author="rzrshr",
     author_email="surplussinewaves@gmail.com",
     description="Digital flashcards",
